beace6dea3b47ed20e46b9480243ee4b12a61c41
Boris Cabrera <boro10194@gmail.com>
2023-07-02 12:30:46 -0400

    creation of models, sync DB with models, server.js configuration

--- models/Product.js
+++ models/Product.js
@@ -6,10 +6,42 @@ const sequelize = require('../config/connection');
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price:{
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true, // Validate that the value is a decimal number
+      },
+    },
+    stock:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true, // Validate that the value is a numeric value
+      },
+    },
+    category_id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "category", // Reference the 'category' table
+        key: "id", // Reference the 'id' column in the 'category  ' table
+
+      },
+    }
   },
   {
     sequelize,
